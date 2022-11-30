8945fc501297c03c66b4d2a727b2e76ad74d6a59
niaopian <2251829764@qq.com>
2022-12-01 00:42:34 +0800

    2022.11.30
    新增C/C++程序基础框架

---
+++ 0.基础框架.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>       // C  中的输入输出流
+
+#include <iostream>      // C++中的输入输出流
+using namespace std;     // C++中cout(),std()等函数命名空间 
+
+int main(void)           // C  中的main函数
+int main()               // C++中的main函数
+{
+
+    
+    system ("pause");    //系统显示“请按任意键结束”
+    return 0;            //main函数返回值   如果是main(void) 最后需要加0吗  不清楚
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# c6
-《C Primer Plus》第6版学习仓库
+# C/C++_code
+《C Primer Plus》第6版 《C++ Primer Plus》同步学习仓库
